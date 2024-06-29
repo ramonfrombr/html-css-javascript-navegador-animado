42f33aaec5520e934efa18a4c193ead6a5f9fec6
Ramon <ramonfrombr@gmail.com>
2024-06-28 21:23:48 -0300

    Código javascript

---
+++ script.js
@@ -0,0 +1,4 @@
+const botao = document.getElementById("botao");
+const navegador = document.getElementById("navegador");
+
+botao.addEventListener("click", () => navegador.classList.toggle("ativo"));
